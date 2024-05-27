c8876375fa8ad2a95fff7d503b9bed48cd339fab
Denys Chernousov <blackInOwls@gmail.com>
2024-05-27 09:46:21 +0300

    gateway with gql

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { AppConfigModule } from '@app/common/app-config/app-config.module';
+import { AppLoggerModule } from '@app/common/app-logger/app-logger.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloGatewayDriver } from '@nestjs/apollo';
+import { ConfigService } from '@nestjs/config';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+	imports: [
+		AppConfigModule,
+		AppLoggerModule,
+		GraphQLModule.forRootAsync<ApolloDriverConfig>({
+			driver: ApolloGatewayDriver,
+			useFactory: (cs: ConfigService) => ({
+				gateway: {
+					supergraphSdl: new IntrospectAndCompose({
+						subgraphs: [
+							{
+								name: 'auth',
+								url: cs.getOrThrow('AUTH_GQL_URL')
+							},
+							{
+								name: 'reservations',
+								url: cs.getOrThrow('RESERVATIONS_GQL_URL')
+							}
+						]
+					})
+				},
+				autoSchemaFile: true,
+				introspection: true,
+				playground: true
+			}),
+			inject: [ConfigService]
+		})
+	],
+	controllers: [],
+	providers: []
 })
-export class GatewayModule {}
+export class GatewayModule {
+}
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(GatewayModule);
+	const configService = app.get(ConfigService);
+	app.use(
+		cookieParser()
+	);
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+			transformOptions: {
+				enableImplicitConversion: true
+			}
+		})
+	);
+	app.useLogger(
+		app.get(Logger)
+	);
+
+	await app.listen(configService.getOrThrow('AUTH_HTTP_PORT'));
+
 }
+
 bootstrap();
