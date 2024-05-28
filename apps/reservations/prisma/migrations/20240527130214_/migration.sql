64079fd0a64420568af7bd5d050fdc373d04706f
Denys Chernousov <blackInOwls@gmail.com>
2024-05-28 15:20:26 +0300

    prisma implementations with different databases

---
+++ apps/reservations/prisma/migrations/20240527130214_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Reservation" (
+    "id" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "invoiceId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("id")
+);
